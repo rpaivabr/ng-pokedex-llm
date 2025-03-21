298a8b448ef104bad7ed7813ca6308a28ceb71cb
Rafael Paiva <r.paivabr@gmail.com>
2025-03-21 20:23:51 -0300

    get api key from prompt

--- src/app/gemini.service.ts
+++ src/app/gemini.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
-import { GoogleGenerativeAI, Part } from '@google/generative-ai';
-
-const API_KEY = localStorage.getItem('API_KEY') || '';
+import { GenerativeModel, GoogleGenerativeAI, Part } from '@google/generative-ai';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GeminiService {
-  private genAI = new GoogleGenerativeAI(API_KEY);
-  private model = this.genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  private genAI!: GoogleGenerativeAI;
+  private model!: GenerativeModel;
+  
+  constructor() {
+    let apiKey = localStorage.getItem('API_KEY') || '';
+    while (!apiKey) {
+      apiKey = prompt("Digite sua API_KEY") || '';
+    }
+    localStorage.setItem('API_KEY', apiKey);
+    this.genAI = new GoogleGenerativeAI(apiKey);
+    this.model = this.genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  }
 
   async generateContent(request: string | Array<string | Part>): Promise<string> {
     const data = await this.model.generateContent(request);
