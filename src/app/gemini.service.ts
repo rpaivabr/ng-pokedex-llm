0d23aef95781e6f1a68fdc37ead9e736beb8cb5d
Rafael Paiva <r.paivabr@gmail.com>
2025-04-26 11:02:58 -0300

    remove local storage

--- src/app/gemini.service.ts
+++ src/app/gemini.service.ts
@@ -9,33 +9,16 @@ export class GeminiService {
   private model!: GenerativeModel;
   
   constructor() {
-    this.initialize();
-  }
-
-  async generateContent(request: string | Array<string | Part>): Promise<string> {
-    try {
-      const data = await this.model.generateContent(request);
-      return data.response.text();
-    } catch (error) {
-      localStorage.removeItem('API_KEY');
-      this.initialize();
-      return Promise.reject("Erro ao gerar conteúdo: " + error);
-    }
-  }
-
-  async initialize(): Promise<void> {
-    let apiKey = localStorage.getItem('API_KEY') || '';
+    let apiKey = '';
     while (!apiKey) {
       apiKey = prompt("Digite sua API_KEY") || '';
     }
-    localStorage.setItem('API_KEY', apiKey);
-    try {
-      this.genAI = new GoogleGenerativeAI(apiKey);
-      this.model = this.genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
-    } catch (error) {
-      localStorage.removeItem('API_KEY');
-      this.initialize();
-      return Promise.reject("Erro ao gerar conteúdo: " + error);
-    }
+    this.genAI = new GoogleGenerativeAI(apiKey);
+    this.model = this.genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  }
+
+  async generateContent(request: string | Array<string | Part>): Promise<string> {
+    const data = await this.model.generateContent(request);
+    return data.response.text();
   }
 }
